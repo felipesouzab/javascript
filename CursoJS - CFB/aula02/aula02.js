a989fbe5c3f60d63fb358c291196c33525dee03b
Felipe de Souza Barbosa <125505965+felipesouzab@users.noreply.github.com>
2023-04-03 17:52:46 -0300

    aula 03js

---
+++ CursoJS - CFB/aula02/aula02.js
@@ -0,0 +1,5 @@
+"use strict" // modo estrito (ou restrito) > necessidade de declarar variáveis e de utilizar todos elas no código
+let nome = "Bruno"
+console.log("CFB Cursos")
+console.log(nome)
+console.log("Nome: " + nome)
---
+++ CursoJS - CFB/aula03/aula03.js
@@ -0,0 +1,25 @@
+"use strict"
+/*
+function teste() {
+    let  nome = "Bruno"
+    if(true) {
+        console.log("dentro do IF teste: " + nome)
+    }
+    console.log("dentro de teste: " + nome)
+}
+teste()
+console.log("fora de teste: " + nome)
+
+*/ 
+let nome = "Bruno"
+nome = "CFB CURSOS"
+nome = 10
+
+const curso = "JavaScript"  // CONSTANTE NÃO PODE SER ALTERADA DURANTE O CÓDIGO
+
+console.log(nome)
+console.log(curso)
+
+
+
+
