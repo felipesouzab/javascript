eb8dd26349c6179b58765b1a52b38b74269fb697
Felipe de Souza Barbosa <125505965+felipesouzab@users.noreply.github.com>
2023-04-04 18:32:03 -0300

    Update aula17.js

--- CursoJS - CFB/aula17/aula17.js
+++ CursoJS - CFB/aula17/aula17.js
@@ -1,10 +1,11 @@
 let n = 0
 let max = 10
-
-for (let i = n; i < max; n++) {
+let pares = 0
+for (let i = n; i < max; i++) {
     console.log("CFB Cursos - " + i )
     if (i % 2 == 0) {
         pares++
+        break
     }
 }
 console.log(`Quantidade de pares - ${pares}`)
