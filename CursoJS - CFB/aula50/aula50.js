0dbd1df3d89e19a1cb8d603dc961c5656e87c889
Felipe de Souza Barbosa <125505965+felipesouzab@users.noreply.github.com>
2023-04-10 11:51:59 -0300

    att

--- CursoJS - CFB/aula50/aula50.js
+++ CursoJS - CFB/aula50/aula50.js
@@ -1,20 +1,12 @@
 const p_array = document.querySelector("#array")
-const btnVerificar = document.querySelector("#btnVerificar")
+const btnReduzir = document.querySelector("#btnReduzir")
 const resultado = document.querySelector("#resultado")
 
-const elementos_array = [16,12,20,10,17,15,13,11,18]
+const elementos_array = [1, 2, 3, 4, 5]
+let aux = []
+
 p_array.innerHTML = "[" + elementos_array + "]"
 
-btnVerificar.addEventListener("click", (evt) => {
-      
-    const ret = elementos_array.some((e, i)=>{
-        if(e<18){
-            resultado.innerHTML = "Array não conforme na posição " + i   
-        }
-        return e >= 18
-    })
-    if (ret) {
-        resultado.innerHTML = "Ok!"
-    }
+btnReduzir.addEventListener("click", (evt)=>{
+    
 })
-
