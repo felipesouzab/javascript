979ed1cbd2d37055bcc903d55470209b0cfefcf7
Felipe de Souza Barbosa <125505965+felipesouzab@users.noreply.github.com>
2023-04-12 16:42:44 -0300

    att

---
+++ DesafiosJS/desafio06.js
@@ -0,0 +1,10 @@
+function myFunction (num) {
+    return num % 2 == 0
+    
+}
+
+
+console.log(myFunction(8))
+console.log(myFunction(-10))
+console.log(myFunction(7))
+console.log(myFunction(-41))
---
+++ DesafiosJS/desafio07.js
@@ -0,0 +1,7 @@
+function myFunction (a,b) {
+    
+}
+
+console.log(myFunction("a"), "Quantas vezes o valor de 'a' ocorre aqui")
+console.log(myFunction("as"), "Quantas vezes o valor de 'a' ocorre aqui")
+console.log(myFunction("q"), "Quantas vezes o valor de 'a' ocorre aqui")
