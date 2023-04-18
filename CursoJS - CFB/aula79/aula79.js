dfe20bd7e6312ced1de3d5565129bbb70313e0e1
Felipe de Souza Barbosa <125505965+felipesouzab@users.noreply.github.com>
2023-04-18 09:55:53 -0300

    projeto alarme

--- CursoJS - CFB/aula79/aula79.js
+++ CursoJS - CFB/aula79/aula79.js
@@ -1,5 +1,43 @@
 const div_data = document.getElementById("div_data")
 const div_relogio = document.getElementById("div_relogio")
+const btn_ativar = document.getElementById("btn_ativar")
+const btn_parar = document.getElementById("btn_parar")
+const tmp_alarme = document.getElementById("tmp_alarme")
+const hora_alarme = document.getElementById("hora_alarme")
+const timer = document.getElementById("timer")
+
+const som_alarme = new Audio("alarme.mp3")
+som_alarme.loop = -1
+
+let ts_atual = null
+let ts_alarme = null
+let alarme_ativado = false 
+let alarme_tocando = false
+
+btn_ativar.addEventListener("click", () => {
+    ts_atual = Date.now()
+    ts_alarme = ts_atual + (tmp_alarme.value*1000)
+    alarme_ativado = true
+    const dt_alarme = new Date (ts_alarme)
+    let h = dt_alarme.getHours()
+    h = h < 10 ? "0" + h : h
+    let m = dt_alarme.getMinutes()
+    m = m < 10 ? "0" + m : m
+    let s = dt_alarme.getSeconds()
+    s = s < 10 ? "0" + s : s
+    hora_alarme.innerHTML = "Hora do Alarme:" + h + ":" + m +":"+ s
+
+})
+
+btn_parar.addEventListener("click", () => {
+    alarme_ativado = false
+    alarme_tocando = false
+    hora_alarme.innerHTML = "Hora do Alarme:"
+    tmp_alarme.value = 0
+    timer.classList.remove("alarme")
+    som_alarme.pause();
+    som_alarme.currentTime = 0;
+})
 
 
 const data = new Date()
@@ -23,6 +61,13 @@ const relogio = () => {
     let segundo = data.getSeconds()
     segundo = segundo < 10 ? "0" + segundo : segundo    
     div_relogio.innerHTML = `Hora: ${hora}` +':'+ `${minuto}` +':'+ `${segundo}`
+    if(alarme_ativado && !alarme_tocando){
+        if(data.getTime() >= ts_alarme){
+            alarme_tocando = true
+            som_alarme.play()
+            timer.classList.add("alarme")
+        }
+    }
 }
 
 const intervalo = setInterval(relogio,1000)
