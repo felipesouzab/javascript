4375bad8be645f0084b95f6f36fd35db473bd043
Felipe de Souza Barbosa <125505965+felipesouzab@users.noreply.github.com>
2023-04-19 18:35:29 -0300

    att

--- CursoJS - CFB/aula87/aula87.js
+++ CursoJS - CFB/aula87/aula87.js
@@ -2,5 +2,12 @@ const url = document.getElementById("url")
 const btn_url = document.getElementById("btn_url")
 
 btn_url.addEventListener("click",(evt)=>{
-    location.href = "https://www.google.com.br"
+    // window.location = "https://google.com.br"
+    // window.location.replace("https://google.com.br")
+    // window.location.assign("https://google.com.br")
+    // window.location.reload()
+    // window.history.forward()
+    // window.history.go(1)
+    // console.log(url.value)
+    // window.location = url.value
 })
