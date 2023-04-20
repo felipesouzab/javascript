69e2c68a5c2c0f902ea8856030ec40f71cb44a44
Felipe de Souza Barbosa <125505965+felipesouzab@users.noreply.github.com>
2023-04-20 09:22:45 -0300

    aula88

--- CursoJS - CFB/aula87/aula87.js
+++ CursoJS - CFB/aula87/aula87.js
@@ -2,7 +2,7 @@ const url = document.getElementById("url")
 const btn_url = document.getElementById("btn_url")
 
 btn_url.addEventListener("click",(evt)=>{
-    // window.location = "https://google.com.br"
+    window.location = "https://google.com.br"
     // window.location.replace("https://google.com.br")
     // window.location.assign("https://google.com.br")
     // window.location.reload()
