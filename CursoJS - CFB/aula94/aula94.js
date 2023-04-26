b1a0cf513ac8a46f8cce46c21ec780b2c5ad5f38
Felipe de Souza Barbosa <125505965+felipesouzab@users.noreply.github.com>
2023-04-26 16:52:40 -0300

    aula94

---
+++ CursoJS - CFB/aula94/aula94.js
@@ -0,0 +1,9 @@
+// import getTodosCursos ,{cursos} from "./cursos2.js";
+import getTodosCursos from "./cursos3.js"
+// import { cursos as c, getCurso as gc} from "./cursos3.js"
+import * as m_cursos from './cursos3.js'
+
+
+console.log(m_cursos.cursos)
+console.log(m_cursos.getCurso([1]))
+console.log(m_cursos.default())
