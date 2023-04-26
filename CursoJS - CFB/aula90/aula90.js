f3270140e80ba4b5c6cac9ee0e49cbdfe8cea359
Felipe de Souza Barbosa <125505965+felipesouzab@users.noreply.github.com>
2023-04-26 15:32:15 -0300

    aula 91 e 92

--- CursoJS - CFB/aula90/aula90.js
+++ CursoJS - CFB/aula90/aula90.js
@@ -1,14 +1,37 @@
 const objetos = document.getElementById("objetos")
 
 const computador = {
-    cpu:"i9",
-    ram:"64gb",
-    hd:"2tb",
+    cpu:"",
+    ram:"",
+    hd:"",
     info:function(){
-        
+        console.log(`CPU: ${this.cpu}`)
+        console.log(`RAM: ${this.ram}`)
+        console.log(`HD: ${this.hd}`)
     }
 
 }
+computador["monitor"] = "22pol"
+computador.placaVideo = "rtx"
+delete(computador.hd)
+
+
+const c1 = Object.assign({},computador)
+
+c1.info()
+
+const c2 = Object.create(computador)
+c2.cpu = "i9"
+c2.ram = "32gb"
+c2.hd = "2TB"
+c2.info()
+
+const o1 = {obj1: "1"}
+const o2 = {obj2: "2"}
+const o3 = {obj3: "3"}
+const o4 = Object.assign(o1,o2,o3)
+console.log(o4)
+
 
 const computadores = [
     {
@@ -28,6 +51,18 @@ const computadores = [
     }
 ]
 
-console.log(computadores)
+computadores.forEach((c)=>{  
+    const div = document.createElement("div")
+    div.innerHTML =  c.cpu + "<br>"+ c.ram + "<br>" + c.hd
+    div.setAttribute("class","computador")
+    objetos.appendChild(div)
+})
 
-objetos.innerHTML = JSON.stringify(computadores)
+
+
+
+
+
+// computador.info()
+// console.log(computadores)
+// objetos.innerHTML = JSON.stringify(computadores)
