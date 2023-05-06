7ffdde39753af125aee3fedd3833e405cb7efb44
Felipe de Souza Barbosa <125505965+felipesouzab@users.noreply.github.com>
2023-05-05 22:15:35 -0300

    att

---
+++ CursoJS - CFB/aula121/aula121.js
@@ -0,0 +1,7 @@
+const f_texto = document.querySelector("#f_texto")
+const p_texto = document.querySelector("#p_texto")
+const btn_texto = document.querySelector("#btn_texto")
+
+btn_texto.addEventListener("click",(evt)=>{
+    
+})
