80c849829a4d5b3b5c149c0fc63ddd50e7e90add
Felipe de Souza Barbosa <125505965+felipesouzab@users.noreply.github.com>
2023-05-08 17:35:42 -0300

    Update aula121.js

--- CursoJS - CFB/aula121/aula121.js
+++ CursoJS - CFB/aula121/aula121.js
@@ -5,3 +5,21 @@ const btn_texto = document.querySelector("#btn_texto")
 btn_texto.addEventListener("click",(evt)=>{
     
 })
+
+let num = 10
+const curso = "JavaScript"
+// localStorage.setItem("nome","bruno")
+// localStorage.setItem("canal","CFB Cursos")
+// localStorage.setItem("curso",curso)
+// localStorage.setItem("numero",num)
+// // alert(localStorage.length)
+// alert(localStorage.getItem(localStorage.key(0)))
+// localStorage.setItem("numero",33)
+// alert(localStorage.getItem("nome"))
+// alert(localStorage.getItem("canal"))
+// alert(localStorage.getItem("curso"))
+localStorage.clear()
+
+sessionStorage.setItem("nome","bruno")
+sessionStorage.setItem("canal","CFB Cursos")
+sessionStorage.setItem("curso",curso)
